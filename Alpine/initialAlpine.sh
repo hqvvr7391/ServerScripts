ab40d74da4220f8c8da82d5b9672df9866b184bc
hqvvr7391 <hqvvr7391@gmail.com>
2022-04-22 15:32:45 +0900

    Alpine3

--- Alpine/initialAlpine.sh
+++ Alpine/initialAlpine.sh
@@ -4,7 +4,7 @@ echo "Initialize Alpine"
 echo "hostname?"
 read hostname
 
-wget https://github.com/hqvvr7391/ServerScript/blob/master/Alpine/initializefile initializefile
+wget https://raw.githubusercontent.com/hqvvr7391/ServerScripts/master/Alpine/initializefile initializefile
 sed -i "s/<%HOSTNAME>/$hostname/g" initializefile
 setup-alpine -f initializefile
 
