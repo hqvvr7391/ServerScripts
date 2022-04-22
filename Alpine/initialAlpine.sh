06cd6a5512cd14c011e49722bc78a6f49237a1be
hqvvr7391 <hqvvr7391@gmail.com>
2022-04-22 16:31:46 +0900

    Alpine4

--- Alpine/initialAlpine.sh
+++ Alpine/initialAlpine.sh
@@ -8,23 +8,4 @@ wget https://raw.githubusercontent.com/hqvvr7391/ServerScripts/master/Alpine/ini
 sed -i "s/<%HOSTNAME>/$hostname/g" initializefile
 setup-alpine -f initializefile
 
-
-/etc/init.d/networking --quiet start &
-
-
-sed -i '3s/# //' /etc/apk/repositories
-
-
-apk add sudo
-echo "username?"
-read username
-adduser "$username"
-echo '%wheel ALL=(ALL) ALL' > /etc/sudoers.d/wheel
-adduser "$username" wheel
-
-apk upgrade --available && sync
-apk update
-
-passwd -l root
-
 reboot
---
+++ Alpine/postinitial.sh
@@ -0,0 +1,29 @@
+#!/bin/ash
+echo "Post Initialize Alpine"
+
+sed -i '3s/# //' /etc/apk/repositories
+
+apk add sudo
+echo "username?"
+read username
+adduser "$username"
+echo '%wheel ALL=(ALL) ALL' > /etc/sudoers.d/wheel
+adduser "$username" wheel
+
+echo "Wanna install qemu-guest-agent? [y/n] : y"
+read qemu
+if[ $qemu = 'y'] 
+then
+    wget https://raw.githubusercontent.com/hqvvr7391/ServerScripts/master/Alpine/pveqemu.sh
+    chmod +x pveqemu.sh
+    ./pveqemu.sh
+    rm pveqemu.sh
+fi
+
+
+apk upgrade --available && sync
+apk update
+
+passwd -l root
+
+reboot
